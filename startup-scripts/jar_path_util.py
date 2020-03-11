afbc8667c1ba7ae2436a9cb29eb2a315e2e45d98
Igor Pisarev <pisarevi@janelia.hhmi.org>
2020-03-11 10:53:04 -0400

    streamline build and startup script for cluster, remove local scripts
    jgo can be used for local runs

--- startup-scripts/jar_path_util.py
+++ startup-scripts/jar_path_util.py
@@ -1,15 +1,9 @@
 import os
 
-def get_local_jar_path():
-	return get_jar_path('-local')
-
-def get_provided_jar_path():
-	return get_jar_path('-provided')
-
 def get_jar_path(jar_suffix=None):
 	curr_script_dir = os.path.dirname(os.path.abspath(__file__))
 	base_folder = os.path.dirname(curr_script_dir)
-	target_folder = os.path.join(base_folder, 'target');
+	target_folder = os.path.join(base_folder, 'target')
 	pom_properties_relpath = os.path.join('maven-archiver', 'pom.properties')
 	pom_properties_path = os.path.join(target_folder, pom_properties_relpath)
 
