1d9469310ffbc0507f2835b89e1858ff9a7d34ec
Caleb Hulbert <cmhulbert@gmail.com>
2025-02-24 15:37:44 -0500

    fix: be more lenient with the default client configuration for N5Factory

--- src/main/kotlin/org/janelia/saalfeldlab/conversion/DefaultFactory.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/conversion/DefaultFactory.kt
@@ -1,11 +1,19 @@
 package org.janelia.saalfeldlab.conversion
 
+import com.amazonaws.ClientConfiguration
 import com.google.gson.GsonBuilder
 import org.janelia.saalfeldlab.n5.universe.N5Factory
 
 private fun defaultGsonBuilder(): GsonBuilder = GsonBuilder().setPrettyPrinting().disableHtmlEscaping()
 internal fun defaultN5Factory(): N5Factory = N5Factory().apply {
 	zarrDimensionSeparator("/")
+	val config = ClientConfiguration().apply {
+		connectionTimeout = 50000
+		socketTimeout = 50000
+		maxConnections = 500
+		maxErrorRetry = 10
+	}
+	s3ClientConfiguration(config)
 	gsonBuilder(defaultGsonBuilder())
 }
 
