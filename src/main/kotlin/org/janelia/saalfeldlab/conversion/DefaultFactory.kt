eadbbb265940762695ba86a09323bfb548393c12
Caleb Hulbert <cmhulbert@gmail.com>
2026-07-09 09:27:45 -0400

    fix: synchronize on the actual open calls for the N5FactoryCache. occasionally had race conditions on a spark cluster.
    really should be upstreamed

--- src/main/kotlin/org/janelia/saalfeldlab/conversion/DefaultFactory.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/conversion/DefaultFactory.kt
@@ -1,7 +1,9 @@
 package org.janelia.saalfeldlab.conversion
 
 import com.google.gson.GsonBuilder
-import org.janelia.saalfeldlab.n5.universe.N5Factory
+import org.janelia.saalfeldlab.n5.KeyValueAccess
+import org.janelia.saalfeldlab.n5.N5Reader
+import org.janelia.saalfeldlab.n5.N5Writer
 import org.janelia.saalfeldlab.n5.universe.N5FactoryWithCache
 import org.janelia.saalfeldlab.n5.universe.StorageFormat
 import software.amazon.awssdk.http.apache.ApacheHttpClient
@@ -10,7 +12,18 @@ import java.time.Duration
 
 private fun defaultGsonBuilder(): GsonBuilder = GsonBuilder().setPrettyPrinting().disableHtmlEscaping()
 
-internal fun defaultN5Factory(): N5Factory = N5FactoryWithCache().apply {
+private class SynchronizedN5FactoryWithCache : N5FactoryWithCache() {
+
+	@Synchronized
+	override fun openReader(storage: StorageFormat?, access: KeyValueAccess, location: URI): N5Reader =
+		super.openReader(storage, access, location)
+
+	@Synchronized
+	override fun openWriter(storage: StorageFormat?, access: KeyValueAccess, location: URI): N5Writer =
+		super.openWriter(storage, access, location)
+}
+
+internal fun defaultN5Factory(): N5FactoryWithCache = SynchronizedN5FactoryWithCache().apply {
 	options { opts ->
 		opts.zarr2 { it.dimensionSeparator("/") }
 		opts.gsonBuilder(defaultGsonBuilder())
